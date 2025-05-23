aa2b50d477860e6d8ef88a8caa161e841ef27fcc
Eldar <tebres.eki76@gmail.com>
2025-05-23 20:40:35 +0200

    Add LOGIN_URL

--- dendo/dendo/settings.py
+++ dendo/dendo/settings.py
@@ -153,6 +153,7 @@ MEDIA_URL = '/content/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'dendo_users.CustomUser'
+LOGIN_URL = 'dendo_users:login_page'
 
 
 log_status = os.getenv('log_lvl', 'INFO').upper()
