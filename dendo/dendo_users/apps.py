185a437e2f633c0a4d269e83e3d6e73cb0cef8db
Eldar <tebres.eki76@gmail.com>
2025-05-23 20:50:34 +0200

    Initialize signals

--- dendo/dendo_users/apps.py
+++ dendo/dendo_users/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class DendoUsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dendo_users'
+
+    def ready(self):
+        from . import signals
